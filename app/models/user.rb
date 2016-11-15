f5c45b5de9aefc30ecb0bfff2aff5623f963923e
ngoctran2512 <trandthuyngoc@gmail.com>
2016-11-15 10:14:56 +0700

    picture update

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,5 @@
 class User < ApplicationRecord
+  has_many :microposts, dependent: :destroy
   validates :name,  presence: true, length: { maximum: 50 }
   VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-]+(\.[a-z\d\-]+)*\.[a-z]+\z/i
   validates :email, presence: true, length: { maximum: 255 },
@@ -7,13 +8,16 @@ class User < ApplicationRecord
 
   has_secure_password
  validates :password, presence: true, length: { minimum: 6 }, allow_nil: true
-
+def feed
+    Micropost.where("user_id = ?", id)
+  end
   class << self
     def digest(string)
         cost = ActiveModel::SecurePassword.min_cost ? BCrypt::Engine::MIN_COST :
                                                   BCrypt::Engine.cost
         BCrypt::Password.create(string, cost: cost)
 	  end
+    
 	def new_token
     SecureRandom.urlsafe_base64
   end
--- app/models/user_test.rb
+++ app/models/user_test.rb
@@ -18,4 +18,10 @@ class UserTest < ActiveSupport::TestCase
   	@user.email = "      "
   	assert_not @user.valid?
   end
+  test "associated microposts should be destroyed" do
+    @user.save
+    @user.microposts.create!(content: "Lorem ipsum")
+    assert_difference 'Micropost.count', -1 do
+      @user.destroy
+    end
 end
